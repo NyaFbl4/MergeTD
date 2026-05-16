e9bbd9fc6b2de31a3b7b9e5d4b934ace7fb51451
IzmaylovNikita <nikitaizmaylov1402@gmail.com>
2026-05-16 12:58:18 +0300

    Add enemyHPbar

--- Assets/Project/Scripts/Gameplay/Enemies/EnemyHealth.cs
+++ Assets/Project/Scripts/Gameplay/Enemies/EnemyHealth.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 namespace Project.Scripts.Gameplay.Enemies
 {
     [RequireComponent(typeof(Collider2D))]
     public class EnemyHealth : MonoBehaviour
     {
+        [Header("Health")]
         [SerializeField] private int _maxHealth = 3;
         private int _currentHealth;
+        
+        [Header("HP Bar")]
+        [SerializeField] private Canvas _hpCanvas;
+        [SerializeField] private Image _hpFill;
+        [SerializeField] private bool _hideWhenFull;
+        [SerializeField] private bool _hideWhenDead = true;
 
         private void Awake()
         {
             _currentHealth = _maxHealth;
+            UpdateHpBar();
         }
 
         public void TakeDamage(int damage)
@@ -20,7 +29,34 @@ namespace Project.Scripts.Gameplay.Enemies
 
             _currentHealth -= damage;
             if (_currentHealth <= 0)
+            {
+                _currentHealth = 0;
+                UpdateHpBar();
+
+                if (_hideWhenDead && _hpCanvas != null)
+                    _hpCanvas.gameObject.SetActive(false);
+
                 Destroy(gameObject);
+                return;
+            }
+            UpdateHpBar();
         }
+        
+        private void UpdateHpBar()
+        {
+            if (_hpFill == null)
+                return;
+
+            var t = _maxHealth <= 0 ? 0f : Mathf.Clamp01((float)_currentHealth / _maxHealth);
+            _hpFill.fillAmount = t;
+
+            if (_hpCanvas != null)
+            {
+                if (_hideWhenFull)
+                    _hpCanvas.gameObject.SetActive(t < 0.999f);
+                else
+                    _hpCanvas.gameObject.SetActive(true);
+            }
+        }        
     }
 }
